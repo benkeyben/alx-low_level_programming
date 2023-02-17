e139cf2c45453cecfe622921faa9c50db98fc869
Benjamin Yankey <benkeyben66@gmail.com>
2023-02-17 09:36:03 -0800

    Add 2-print_alphabet.c file which prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+*/
+/* Program that prints the alphabet in lowercase */
+int main(void)
+{
+	char str_lower_case = 'S';
+
+	putchar(tolower(str_lower_case));
+	putchar('\n');
+	return (0);
+}
