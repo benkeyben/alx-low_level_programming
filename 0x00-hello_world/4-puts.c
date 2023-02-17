c367cff55cbe7418aae0b8430e4bd318f14e4253
Benjamin Yankey <benkeyben66@gmail.com>
2023-02-16 16:16:41 -0800

    4-puts.c files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,11 +6,11 @@
  * *
  * *
  * *Return: return 0 everytime
- * */
+*/
 int main(void)
 {
-
 	char displayed[52];
+
 	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
 	puts(displayed);
 	return (0);
