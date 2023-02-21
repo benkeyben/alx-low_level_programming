c9df751bbc7a9063753430a065cfa8b600fa639d
Benjamin Yankey <benkeyben66@gmail.com>
2023-02-21 13:02:14 -0800

    Add 5-sign.c 5-sign files

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
-/**
+int print_last_digit(int n);/**
 *File:main.h
-*Author: Benjamin Yankey
+*Author: Benjamin Yanikey
 *Desc:Header file containing functions declarations
 */
 
@@ -9,3 +9,11 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
