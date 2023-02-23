de72a4bedc46ec15ca57f8b8063359af38cdcf62
Benjamin Yankey <benkeyben66@gmail.com>
2023-02-23 13:47:37 -0800

    Modify 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,13 +16,14 @@ int main(void)
 	{
 
 		if (i % 15 == 0)
-			printf("FizzBuzz\t");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz\t");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz\t");
+			printf("Buzz");
 		else
-			printf("%d\t", i);
+			printf("%d", i);
+		printf("%c", ' ');
 	}
 	printf("\n");
 	return (0);
