43d93faacdb6c959a0a0433b54a7b14ff8a29ba0
Benjamin Yankey <benkeyben66@gmail.com>
2023-02-23 13:25:35 -0800

    Modify .gitignore and main.h: Add 10101-print_number.c file

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - Prints an integer
+ * @n: The number to be checked
+ *
+*/
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+	_putchar(num % 10 + '0');
+}
+
