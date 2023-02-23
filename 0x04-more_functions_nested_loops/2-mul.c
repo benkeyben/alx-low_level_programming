68f7583db6d3f8617e863483740e23ac388ad0d6
Benjamin Yankey <benkeyben66@gmail.com>
2023-02-23 04:05:08 -0800

    Add 2-mul.c, modify main.h and .gitignore files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - A function tha multiplies two integers
+ *@a: first parameter
+ *@b: Second parameter
+ *Return: return a * b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
