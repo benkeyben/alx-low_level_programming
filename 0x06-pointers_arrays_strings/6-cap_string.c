d18cb4d1b9b7f85b83cf17e28fce0ee0450787b4
Benjamin Yankey <benkeyben66@gmail.com>
2023-03-02 08:00:16 -0800

    Modify 6-cap_string.c file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,32 @@
 #include "main.h"
-
 /**
- * *cap_string - capitalizes all words of a string
- * @cap: words of a string to be capitalized
- * Return: return the capitalized words of a string
- */
-char *cap_string(char *cap)
+*cap_string - function that capitalize first character of a word
+*@str: string to capitalize
+*Return:returns the capitalized string
+*/
+char *cap_string(char *str)
 {
-	int i, str_len, next_char;
+	int index = 0;
 
-	str_len = 0;
-	for (i = 0; cap[i] != '\0'; i++)
-		str_len++;
-
-	for (i = 0; i < str_len; i++)
+	while (str[++index])
 	{
-		if ((cap[i] >= 'A' && cap[i] <= 'Z') || (cap[i] >= 'a' && cap[i] <= 'z'))
-		{
-			cap[i] = cap[i];
-		}
-		else
-		{
-			if (cap[i] == '\t')
-				cap[i] = ' ';
-			cap[i] = cap[i];
-			next_char = i + 1;
-			if (cap[next_char] >= 'A' && cap[next_char] <= 'Z')
-				cap[next_char] = cap[next_char];
-			else if (cap[next_char] >= 'a' && cap[next_char] <= 'z')
-				cap[next_char] -= 32;
-		}
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}')
+			str[index] -= 32;
 	}
-	return (cap);
+	return (str);
 }
