5a86a2bd9e179b56bb1d83c8dc812a0f3ea2f221
Benjamin Yankey <benkeyben66@gmail.com>
2023-03-06 05:04:01 -0800

    Add main.h, .gitignore and 0-memset.c files

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+/**
+ * Description - Header file containing function prototypes
+ * Author: Benjamin Yankey
+*/
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
