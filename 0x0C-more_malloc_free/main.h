8eb230763044c4b0715c63e9a2af29417e367bbf
Benjamin Yankey <benkeyben66@gmail.com>
2023-03-16 14:07:16 -0700

    Modify main.h and 101-mul.c files

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define min(x, y) (((x) < (y)) ? (x) : (y))
 #include <stdio.h>
 #include <stdlib.h>
+
+#define min(x, y) (((x) < (y)) ? (x) : (y))
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
