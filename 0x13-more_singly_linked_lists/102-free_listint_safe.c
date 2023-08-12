8d1d9c4c7d57b23657d54dd633ccc5084408421c
Benjamin Yankey <benkeyben66@gmail.com>
2023-08-12 13:23:33 +0000

    Modify advanced tasks of the project

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,29 +1,6 @@
 #include "lists.h"
 
 /**
- * free_listp2 - frees a linked list
- * @head: head of a list.
- *
- * Return: no return.
- */
-void free_listp2(listp_t **head)
-{
-	listp_t *temp;
-	listp_t *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
  * free_listint_safe - frees a linked list.
  * @h: head of a list.
  *
